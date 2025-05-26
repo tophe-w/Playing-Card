087685aece13437363a78705760471e068ca7ea5
tophe-w <tophe1306@gmail.com>
2025-05-26 15:27:31 +0200

    materials Angular

---
+++ Front/src/app/components/delete-monster-confirmation-dialog/delete-monster-confirmation-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-monster-confirmation-dialog',
+  imports: [],
+  templateUrl: './delete-monster-confirmation-dialog.component.html',
+  styleUrl: './delete-monster-confirmation-dialog.component.scss'
+})
+export class DeleteMonsterConfirmationDialogComponent {
+
+}
